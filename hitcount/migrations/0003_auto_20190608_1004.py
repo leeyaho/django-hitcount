10d9fc0135ea8c3b7b83512fc1779090c45e3f08
juhun lee <leeyaho@gmail.com>
2019-08-08 16:22:33 +0900

    change field type

--- hitcount/migrations/0003_auto_20190608_1004.py
+++ hitcount/migrations/0003_auto_20190608_1004.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='hitcount',
             name='object_pk',
-            field=models.TextField(verbose_name='object ID'),
+            field=models.IntegerField(verbose_name='object ID'),
         ),
     ]
